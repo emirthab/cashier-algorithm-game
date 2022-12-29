907e1993b0c28786bf79eb36ee139593d1590479
Emir <52050284+emirthab@users.noreply.github.com>
2022-12-29 20:01:02 +0300

    coins unshaded

--- scripts/case.gd
+++ scripts/case.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-@onready var moneys = [10,10, 5, 5,5,5,5,5,5, 4, 5, 4, 3, 2, 1, 0.5,0.5, 0.25, 0.10]
-@onready var change = 10
+@onready var moneys : Array
+@onready var change : float = 10
 var cashier_algorithm = preload("res://scripts/cashier_algorithm.gd")
 
 func _ready():
+	set_moneys([10,10, 5, 5,5,5,5,5,5, 4, 5, 4, 3, 2, 1, 0.5,0.5, 0.25, 0.10])
 	var moneys_dict = cashier_algorithm.convert_coins_dict(moneys)
 	var combos = cashier_algorithm.give_change(change, moneys_dict)
 	cashier_algorithm.sort_arrays_by_size(combos)
@@ -13,3 +14,6 @@ func _ready():
 
 func _process(delta):
 	pass
+
+func set_moneys(moneys : Array):
+	self.moneys = moneys
